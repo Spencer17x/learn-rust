bbb46fea3546646cfb719ff8f8345d89b51cff34
Spencer17x <1253478653@qq.com>
2021-08-20 16:34:22 +0800

    feat: add ignore file

--- guessing_game/target/rls/debug/deps/guessing_game-ccd0c1c867530d5f.d
+++
@@ -1,5 +0,0 @@
-/Users/17a/Desktop/repo/learn-rust/guessing_game/target/rls/debug/deps/guessing_game-ccd0c1c867530d5f.rmeta: src/main.rs
-
-/Users/17a/Desktop/repo/learn-rust/guessing_game/target/rls/debug/deps/guessing_game-ccd0c1c867530d5f.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/rls/debug/deps/guessing_game-e154f988684f57a6.d
+++
@@ -1,5 +0,0 @@
-/Users/17a/Desktop/repo/learn-rust/guessing_game/target/rls/debug/deps/guessing_game-e154f988684f57a6.rmeta: src/main.rs
-
-/Users/17a/Desktop/repo/learn-rust/guessing_game/target/rls/debug/deps/guessing_game-e154f988684f57a6.d: src/main.rs
-
-src/main.rs:
